1cf7a555034235e052cd559e696099ff391ca697
AmedeoPioSpina <spina.amedeo.pio@gmail.com>
2024-02-05 19:35:01 +0100

    app blade edited

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,21 +1,22 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>@yield('title', 'Template')</title>
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    @vite('resources/js/app.js')
-    @yield('head-content')
-</head>
-<body>
-    @include('partials.header')
-    <main>
-        @yield('main-content')
-    </main>
-    @include('partials.footer')
-</body>
+        <title>@yield("page-title","Laravel DC Comics")</title>
+
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css2?family=Open+Sans+Condensed:wght@300;700&family=Open+Sans:wght@300;400;600;700&display=swap" rel="stylesheet">
+
+        {{-- STYLE --}}
+        <link rel="stylesheet" href="{{ asset("css/app.css") }}">
+
+    </head>
+    <body>
+        @include("partials.header")
+        @yield("content")
+        @include("partials.footer")
+    </body>
 </html>
